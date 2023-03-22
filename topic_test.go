61d80ab762a053c2492de90781b958d47f8550df
what's for dinner <cruelangeleo7@gmail.com>
2023-03-22 23:04:23 +1100

    #2 added tests for existing SNS functionality, unit tests are doneso

---
+++ topic_test.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/sns"
+)
+
+type SNSAPIImpl struct{}
+
+func (c SNSAPIImpl) Subscribe(ctx context.Context,
+	params *sns.SubscribeInput,
+	optFns ...func(*sns.Options)) (*sns.SubscribeOutput, error) {
+	if *params.TopicArn == "valid-topic" {
+		return &sns.SubscribeOutput{
+			SubscriptionArn: aws.String("foo:bar:baz"),
+		}, nil
+	}
+	return nil, errors.New("Couldn't subscribe to topic")
+}
+
+func (c SNSAPIImpl) Unsubscribe(ctx context.Context,
+	params *sns.UnsubscribeInput,
+	optFns ...func(*sns.Options)) (*sns.UnsubscribeOutput, error) {
+	if *params.SubscriptionArn == "valid:arn" {
+		return &sns.UnsubscribeOutput{}, nil
+	}
+
+	return nil, errors.New("Could not unsubscribe using that ARN")
+}
+
+func TestSubscribe(t *testing.T) {
+	tests := map[string]struct {
+		shouldErr   bool
+		topicArn    *string
+		expectedArn string
+	}{
+		"valid input":   {false, aws.String("valid-topic"), "foo:bar:baz"},
+		"invalid input": {true, aws.String("invalid-topic"), ""},
+	}
+
+	client := &SNSAPIImpl{}
+	ctx := context.TODO()
+	queueArn := aws.String("some-valid-queue")
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {})
+		result, err := subscribeToTopic(ctx, client, test.topicArn, queueArn)
+
+		if err != nil && !test.shouldErr {
+			t.Fatalf(
+				"Expected no error but got %s",
+				err.Error(),
+			)
+		}
+
+		if err == nil && test.shouldErr {
+			t.Fatalf("Expected error but got no error")
+		}
+
+		if err == nil && !test.shouldErr {
+			if *result != test.expectedArn {
+				t.Fatalf(
+					"Subscription ARN %s did not match expected ARN %s",
+					*result,
+					test.expectedArn,
+				)
+			}
+		}
+	}
+}
+
+func TestUnsubscribe(t *testing.T) {
+	tests := map[string]struct {
+		shouldErr       bool
+		subscriptionArn *string
+	}{
+		"valid subscription ARN":   {false, aws.String("valid:arn")},
+		"invalid subscription ARN": {true, aws.String("invalid:arn")},
+	}
+
+	ctx := context.TODO()
+	client := &SNSAPIImpl{}
+
+	for name, test := range tests {
+		t.Run(name, func(t *testing.T) {
+			err := unsubscribeFromTopic(ctx, client, test.subscriptionArn)
+
+			if err != nil && !test.shouldErr {
+				t.Fatalf(
+					"Expected no error but got %s",
+					err.Error(),
+				)
+			}
+
+			if err == nil && test.shouldErr {
+				t.Fatal("Expected error but got no error")
+			}
+		})
+	}
+}
